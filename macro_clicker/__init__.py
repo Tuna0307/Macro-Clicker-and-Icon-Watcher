d1642e45f9f77525dc4af06cf791e3f54d3c05f7
Wei Xuan <2401193@sit.singaporetech.edu.sg>
2026-09-04 16:15:23 +0800

    feat(rally): install v18 stale count guard

--- macro_clicker/__init__.py
+++ macro_clicker/__init__.py
@@ -50,6 +50,10 @@ from .rally_hot_path_v17_runtime import install_rally_hot_path_v17_runtime
 
 install_rally_hot_path_v17_runtime()
 
+from .rally_hot_path_v18_runtime import install_rally_hot_path_v18_runtime
+
+install_rally_hot_path_v18_runtime()
+
 del install_rally_three_team_runtime
 del install_rally_hot_path_runtime
 del install_rally_hot_path_v7_runtime
@@ -63,3 +67,4 @@ del install_rally_hot_path_v14_runtime
 del install_rally_hot_path_v15_runtime
 del install_rally_hot_path_v16_runtime
 del install_rally_hot_path_v17_runtime
+del install_rally_hot_path_v18_runtime
